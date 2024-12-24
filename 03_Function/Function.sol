c6cf98a206810c397500bb34f922141c3b937f1e
john03690248 <71982647+john03690248@users.noreply.github.com>
2024-12-24 20:11:07 +0800

    Update Function.sol

--- 03_Function/Function.sol
+++ 03_Function/Function.sol
@@ -5,34 +5,26 @@ contract FunctionTypes{
     
     constructor() payable {}
 
-    // 函数类型
-    // function (<parameter types>) {internal|external} [pure|view|payable] [returns (<return types>)]
-    // 默认function
     function add() external{
         number = number + 1;
     }
 
-    // pure: 纯纯牛马
     function addPure(uint256 _number) external pure returns(uint256 new_number){
         new_number = _number+1;
     }
-    
-    // view: 看客
+
     function addView() external view returns(uint256 new_number) {
         new_number = number + 1;
     }
 
-    // internal: 内部函数
     function minus() internal {
         number = number - 1;
     }
 
-    // 合约内的函数可以调用内部函数
     function minusCall() external {
         minus();
     }
 
-    // payable: 递钱，能给合约支付eth的函数
     function minusPayable() external payable returns(uint256 balance) {
         minus();    
         balance = address(this).balance;
